26e46f75e8c2429493b75712f7ad94edf97002fc
Apong <Apong@aj>
2017-07-06 08:24:48 +0530

    first commit

---
+++ src/com/test/Run.java
@@ -0,0 +1,32 @@
+package com.test;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Run{
+	
+	public static void main(String[] args) throws IOException{
+		String filename = System.getProperty("user.dir") + "//resources//build.txt";
+
+		File file = new File(filename);
+
+		if (!file.exists()) {
+			file.createNewFile();
+		}	
+		Boolean append_to_file = true;
+		FileWriter writer = new FileWriter(file.getAbsolutePath(), append_to_file);
+		BufferedWriter bw = new BufferedWriter(writer);
+		bw.write("**********************************************\n");
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S");
+		bw.write("Built on: " + sdf.format(date) + "\n"); 		
+
+		bw.close();
+		writer.close();
+
+	}
+}
